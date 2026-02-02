f069c9ab596cd4cdb3334729160033dcb717f1c6
Sazzadur Rahman <sazzadur570@gmail.com>
2026-02-02 23:32:59 +0600

    added new items

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
 from .views import(
-    home,
-    login
+    HomeView,
+    LoginView,
+    LogoutView,
+    RegistrationView
 )
 
 urlpatterns = [
-    path("",home.as_view(), name="home"),
-    path("login/",login.as_view(), name="login")
+    path("",HomeView.as_view(), name="home"),
+    path("login/",LoginView.as_view(), name="login"),
+    path("logout/",LogoutView.as_view(), name="logout"),
+    path("registration/",RegistrationView.as_view(), name="registration")
 ]
--- account/views.py
+++ account/views.py
@@ -1,17 +1,19 @@
 from django.views import generic
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 from .forms import UserLoginForm
 
 #view start from hare
 #home page view
-class home(PermissionRequiredMixin, generic.TemplateView):
+class HomeView(LoginRequiredMixin, generic.TemplateView):
     template_name = "account/index.html"
 
 
 #login page view
-class login(generic.View):
+class LoginView(generic.View):
     def get(self, *args, **kwargs):
         form = UserLoginForm()
         context ={
@@ -20,4 +22,34 @@ class login(generic.View):
         return render(self.request, 'account/login.html', context)
     
     def post(self, *args, **kwargs):
-        pass
+        form = UserLoginForm(self.request.POST)
+        
+        if form.is_valid():
+            user = authenticate(
+                self.request,
+                username = form.cleaned_data.get('username'),
+                password = form.cleaned_data.get('password')
+            )
+            if user:
+                login(self.request,user)
+                return redirect('home')
+            else:
+                messages.warning(self.request,"Wrong Credential!")
+                return redirect('login')
+        context = {
+            "form":form
+        }
+            
+        return render(self.request, "account/login.html", context)
+
+
+# logout view
+class LogoutView(generic.View):
+    def get(self, *args, **kwargs):
+        logout(self.request)
+        return redirect('login')
+
+
+# registration view
+class RegistrationView(generic.TemplateView):
+    template_name = 'account/registration.html'
